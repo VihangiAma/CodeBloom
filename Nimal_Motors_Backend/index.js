82518dbc1276c32bc566fa1931a14f84c198b45e
mpwijesiri <maheshika_20211083@fot.ruh.ac.lk>
2025-03-14 23:37:55 +0530

    body-parser install

--- Nimal_Motors_Backend/index.js
+++ Nimal_Motors_Backend/index.js
@@ -1,12 +1,22 @@
+import bodyParser from "body-parser"
 import express from "express"
 
 const app = express()
+app.use(bodyParser.json())
 
 app.get("/",(req,res)=>{
  console.log("Hello world")
+ res.json({
+    message :"hi"
+ })
 })
 app.post("/",(req,res)=>{
+    const name =req.body.name
+    const message ="hi " + " "+ name
     console.log("this is post request")
+    res.json({
+        message:message
+    })
 })
 
 app.listen(5000,(req,res)=>{
