4436ca79d19a7f77c98f039821989a27918d8c9b
mpwijesiri <maheshika_20211083@fot.ruh.ac.lk>
2025-03-14 23:59:54 +0530

    comments

--- Nimal_Motors_Backend/index.js
+++ Nimal_Motors_Backend/index.js
@@ -4,20 +4,20 @@ import express from "express"
 const app = express()
 app.use(bodyParser.json())
 
-app.get("/",(req,res)=>{
+/*app.get("/",(req,res)=>{
  console.log("Hello world")
  res.json({
     message :"hi"
  })
-})
-app.post("/",(req,res)=>{
+})*/
+/*app.post("/",(req,res)=>{
     const name =req.body.name
     const message ="hi " + " "+ name
     console.log("this is post request")
     res.json({
         message:message
     })
-})
+})*/
 
 app.listen(5000,(req,res)=>{
     console.log("sever is running port 5000")
