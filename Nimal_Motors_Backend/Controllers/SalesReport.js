a1f593b4d835545d2487670cac1d89b1988f83a9
mpwijesiri <maheshika_20211083@fot.ruh.ac.lk>
2025-03-29 21:13:42 +0530

    Create Sales Report Delete Function

--- Nimal_Motors_Backend/Controllers/SalesReport.js
+++ Nimal_Motors_Backend/Controllers/SalesReport.js
@@ -61,3 +61,33 @@ export async function CreateSalesReport(req,res) {
        });
    }
  }
+
+ //Delete function
+ export async function updateSalesReport(req, res) {
+   try {
+       const { id } = req.params; // User ID from the URL parameters
+       const updatedData = req.body; // Data to update
+ 
+       const updatedReport = await SalesReport.findOneAndUpdate(
+           { itemId: id }, // Search for the report with this User ID
+           updatedData, // New data to update
+           { new: true } // Return the updated document
+       );
+ 
+       if (!updatedReport) {
+           return res.status(404).json({
+               message: "Report not found"
+           });
+       }
+       res.status(200).json({
+         message: "Report updated successfully",
+         report: updatedReport
+     });
+ } catch (error) {
+     res.status(500).json({
+         message: "Failed to update report",
+         error: error.message
+     });
+ }
+ }
+ 
--- Nimal_Motors_Backend/Routers/SalesReport.js
+++ Nimal_Motors_Backend/Routers/SalesReport.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import{CreateSalesReport,getSalesReport,deleteSalesReport}from '../Controllers/SalesReport.js'
+import{CreateSalesReport,getSalesReport,deleteSalesReport, updateSalesReport}from '../Controllers/SalesReport.js'
 
 const SalesRoter = express.Router();
 
 SalesRoter.post("/",CreateSalesReport);
 SalesRoter.get("/",getSalesReport);
 SalesRoter.delete("/:id",deleteSalesReport);
+SalesRoter.put("/:id",updateSalesReport);
 
 export default SalesRoter;
