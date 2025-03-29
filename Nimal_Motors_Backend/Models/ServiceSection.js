f1d7439c0eaae9d8d0a439f89d7199f2719b0bb2
H.M.D. Anuruddhika <143576896+AnuHewage@users.noreply.github.com>
2025-03-29 14:24:59 +0530

    Create Service Section

---
+++ Nimal_Motors_Backend/Models/ServiceSection.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const serviceSchema = new mongoose.Schema({
+  serviceID: { type: String, required: true, unique: true },
+  customerID: { type: mongoose.Schema.Types.ObjectId, ref: "Customer", required: true },
+  vehicleID: { type: mongoose.Schema.Types.ObjectId, ref: "Vehicle", required: true },
+  serviceType: { 
+    type: String, 
+    enum: ["Mechanical", "Electrical", "Bodyshop"], 
+    required: true 
+  },
+  serviceDetails: { type: String, required: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date },
+  status: { type: String, enum: ["Pending", "In Progress", "Completed"], default: "Pending" },
+  totalCost: { type: Number, required: true }
+});
+
+module.exports = mongoose.model("ServiceSection", serviceSchema);
