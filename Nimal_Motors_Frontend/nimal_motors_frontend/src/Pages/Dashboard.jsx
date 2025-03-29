e5572eaf25b9f37deea405a68f3dfe56b99ca58e
K.V.A.T Nishshanka <129648585+kvat001@users.noreply.github.com>
2025-03-29 15:47:51 +0530

    update previous structure

---
+++ Nimal_Motors_Frontend/nimal_motors_frontend/src/Pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import InventoryTable from "../Components/InventoryTable";
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Inventory Dashboard</h1>
+            <InventoryTable />
+        </div>
+    );
+};
+
+export default Dashboard;
