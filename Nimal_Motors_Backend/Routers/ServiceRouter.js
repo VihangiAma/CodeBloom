1224ebe1aca490a65e8739a7336b31ad9abb4b93
H.M.D. Anuruddhika <143576896+AnuHewage@users.noreply.github.com>
2025-03-29 15:10:14 +0530

    Create ServiceRouter.js

---
+++ Nimal_Motors_Backend/Routers/ServiceRouter.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+import{
+    createService,
+    getAllServices,
+    updateService,
+    deleteService
+}
+
+from" ../Controllers/serviceController.js";
+const serviceRouter = express.Router();
+
+
+serviceRouter.post("/", createService);
+serviceRouter.get("/", getAllServices);
+serviceRouter.put("/:id", updateService);
+serviceRouter.delete("/:id", deleteService);
+
+
+export default serviceRouter;
