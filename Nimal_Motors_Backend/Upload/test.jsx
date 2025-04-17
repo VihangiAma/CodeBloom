e3d99e07bb32a32c5db2e094ba745dec0fa72d0d
H.M.D. Anuruddhika <143576896+AnuHewage@users.noreply.github.com>
2025-04-17 10:08:51 +0530

    Update test.jsx

--- Nimal_Motors_Backend/Upload/test.jsx
+++ Nimal_Motors_Backend/Upload/test.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function App() {
-  return <h1>Hello, World!</h1>;
+ // return <h1>Hello, World!</h1>;
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a simple React component.</p>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
