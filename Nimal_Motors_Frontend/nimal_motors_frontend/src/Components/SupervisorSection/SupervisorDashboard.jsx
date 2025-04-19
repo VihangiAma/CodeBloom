ea4537c3179fc751a8ce71f7e38f4bf8a0fac789
H.M.D. Anuruddhika <143576896+AnuHewage@users.noreply.github.com>
2025-04-19 17:15:17 +0530

    Update SupervisorDashboard.jsx

--- Nimal_Motors_Frontend/nimal_motors_frontend/src/Components/SupervisorSection/SupervisorDashboard.jsx
+++ Nimal_Motors_Frontend/nimal_motors_frontend/src/Components/SupervisorSection/SupervisorDashboard.jsx
@@ -38,7 +38,7 @@ function Sidebar() {
           <span>📅 Appointment</span>
         </a> */}
         <Link to="/appointments" className="block p-2 hover:bg-gray-100 rounded">
-  📅  Appointments
+  📅  Manage Appointments
         </Link>
       </nav>
     </aside>
