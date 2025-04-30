1793736841ad0ce3451f974df521aad061fbf680
Subhani-Jayoda <subhagamage100@gmail.com>
2025-04-30 18:25:55 +0530

    updated

--- Nimal_Motors_Backend/Models/db.js
+++ Nimal_Motors_Backend/Models/db.js
@@ -7,15 +7,12 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      //useNewUrlParser: true,
-      //useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected...");
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("✅ MongoDB connected successfully...");
   } catch (err) {
-    console.error("Error connecting to MongoDB:", err);
-    process.exit(1);
+    console.error("❌ Error connecting to MongoDB:", err.message);
+    process.exit(1); // Exit the app if DB connection fails
   }
 };
 
-export default connectDB; // Default export
+export default connectDB;
