8e5c55f58fca112ea78b7e14191788b15f57ba64
H.M.D. Anuruddhika <143576896+AnuHewage@users.noreply.github.com>
2025-05-04 09:05:29 +0530

    Update AppointmentRoutes.js

--- Nimal_Motors_Backend/Routers/AppointmentRoutes.js
+++ Nimal_Motors_Backend/Routers/AppointmentRoutes.js
@@ -1,18 +1,48 @@
-import express from "express";
+// import express from "express";
 
-import{
-    createAppointment,
-    getAppointments,
-    updateAppointment,
-    deleteAppointment
-} from "../Controllers/AppointmentController.js";
+// import{
+//     createAppointment,
+//     getAllAppointments,
+//     getAppointments,
+//     updateAppointment,
+//     deleteAppointment
+// } from "../Controllers/AppointmentController.js";
 
-const appointmentRouter = express.Router();
+// const appointmentRouter = express.Router();
 
 
-appointmentRouter.post("/", createAppointment);
-appointmentRouter.get("/", getAppointments);
-appointmentRouter.put("/:id", updateAppointment);
-appointmentRouter.delete("/:id", deleteAppointment);
+// appointmentRouter.post("/", createAppointment);
+// appointmentRouter.get("/", getAppointments);
+// appointmentRouter.get("/:id", getAllAppointments);
+// appointmentRouter.put("/:id", updateAppointment);
+// appointmentRouter.delete("/:id", deleteAppointment);
 
-export default appointmentRouter;
+// export default appointmentRouter;
+
+import express from 'express';
+import {
+  createAppointment,
+  getAllAppointments,
+  getAppointmentById,
+  updateAppointment,
+  deleteAppointment
+} from '../Controllers/appointmentController.js';
+
+const router = express.Router();
+
+// Create a new appointment
+router.post('/', createAppointment);
+
+// Get all appointments
+router.get('/', getAllAppointments);
+
+// Get a single appointment by MongoDB _id
+router.get('/:id', getAppointmentById);
+
+// Update an appointment by serviceID (custom numeric ID)
+router.put('/:id', updateAppointment);
+
+// Delete an appointment by serviceID
+router.delete('/:id', deleteAppointment);
+
+export default router;
