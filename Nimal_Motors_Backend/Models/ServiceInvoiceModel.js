87ad0202bf59ec9db9cd5101f21c231593616fed
H.M.D. Anuruddhika <143576896+AnuHewage@users.noreply.github.com>
2025-05-12 10:02:14 +0530

    Update ServiceInvoiceModel.js

--- Nimal_Motors_Backend/Models/ServiceInvoiceModel.js
+++ Nimal_Motors_Backend/Models/ServiceInvoiceModel.js
@@ -1,43 +1,32 @@
-// // const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
-// // const serviceInvoiceSchema = new mongoose.Schema({
-// //   serviceID: { type: String, required: true, unique: true },
-// //   customerName: { type: String, required: true },
-// //   vehicleNumber: { type: String, required: true },
-// //   vehicleType: { type: String, required: true },
-// //   contactPhone: { type: String, required: true },
-// //   description: { type: String },
-// //   services: {
-// //     fullService: { selected: Boolean, cost: Number },
-// //     bodyWash: { selected: Boolean, cost: Number },
-// //     oilChange: { selected: Boolean, cost: Number },
-// //     underbodyWash: { selected: Boolean, cost: Number },
-// //     interiorVacuum: { selected: Boolean, cost: Number },
-// //   },
-// //   totalCost: { type: Number, required: true },
-// // });
+const serviceInvoiceSchema = new mongoose.Schema({
+  serviceID: { type: String, required: true, unique: true },
+  customerName: { type: String, required: true },
+  vehicleNumber: { type: String, required: true },
+  vehicleType: { type: String, required: true },
+  contactPhone: { type: String, required: true },
+  description: { type: String },
+  section: {String},
+  services: {
+    fullService: { selected: Boolean, cost: Number },
+    bodyWash: { selected: Boolean, cost: Number },
+    oilChange: { selected: Boolean, cost: Number },
+    underbodyWash: { selected: Boolean, cost: Number },
+    interiorVacuum: { selected: Boolean, cost: Number },
+  },
+  items: [
+    {
+      description: String,
+      qty: Number,
+      cost: Number,
+    }
+  ],
+  totalCost: { type: Number, required: true },
+  submittedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  isApproved: { type: Boolean, default: false },
+  adminRemarks: String,
+  createdAt: { type: Date, default: Date.now },
+});
 
-// // const ServiceInvoice = mongoose.model('ServiceInvoice', serviceInvoiceSchema);
-
-// // module.exports = ServiceInvoice;
-
-// import mongoose from "mongoose";
-
-// const serviceInvoiceSchema = new mongoose.Schema({
-//   serviceID: { type: String, required: true, unique: true },
-//   customerName: { type: String, required: true },
-//   vehicleNumber: { type: String, required: true },
-//   vehicleType: { type: String, required: true },
-//   contactPhone: { type: String, required: true },
-//   description: { type: String },
-//   services: {
-//     fullService: { selected: Boolean, cost: Number },
-//     bodyWash: { selected: Boolean, cost: Number },
-//     oilChange: { selected: Boolean, cost: Number },
-//     underbodyWash: { selected: Boolean, cost: Number },
-//     interiorVacuum: { selected: Boolean, cost: Number },
-//   },
-//   totalCost: { type: Number, required: true },
-// });
-
-// export default mongoose.model('ServiceInvoice', serviceInvoiceSchema, 'ServiceInvoice');
+export default mongoose.model('ServiceInvoice', serviceInvoiceSchema, 'ServiceInvoice');
