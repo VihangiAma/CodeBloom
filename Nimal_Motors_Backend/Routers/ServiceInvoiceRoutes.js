4c6fef2a73e2fdeac243d390ed5230369628dcc4
H.M.D. Anuruddhika <143576896+AnuHewage@users.noreply.github.com>
2025-05-13 23:54:37 +0530

    Update ServiceInvoiceRoutes.js

--- Nimal_Motors_Backend/Routers/ServiceInvoiceRoutes.js
+++ Nimal_Motors_Backend/Routers/ServiceInvoiceRoutes.js
@@ -10,7 +10,7 @@ import {
 const router = express.Router();
 
 // Routes
-router.post('/api/invoice', createInvoice);
+router.post('/', createInvoice);
 router.get('/', getAllInvoices);
 router.get('/:id', getInvoiceById);
 router.put('/:id/approve', updateInvoiceApproval);
