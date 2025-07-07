edf97b7219720cee935a273e428acab110c74666
mpwijesiri <maheshika_20211083@fot.ruh.ac.lk>
2025-07-07 12:08:00 +0530

    update to remark

--- Nimal_Motors_Frontend/src/pages/admin/AdminInvoiceView.jsx
+++ Nimal_Motors_Frontend/src/pages/admin/AdminInvoiceView.jsx
@@ -16,8 +16,7 @@ const AdminInvoiceView = () => {
         const formattedData = data.map(invoice => ({
           ...invoice,
           totalCost: invoice.totalCost || 0,
-          adminRemarks: invoice.adminRemarks || "N/A",
-          description: invoice.description || "N/A" // Added description field
+          adminRemarks: invoice.adminRemarks || "N/A"
         }));
         
         setInvoices(formattedData);
@@ -91,7 +90,6 @@ const AdminInvoiceView = () => {
                 <th className="p-3 border border-gray-300">Service ID</th>
                 <th className="p-3 border border-gray-300">Customer Name</th>
                 <th className="p-3 border border-gray-300">Section</th>
-                <th className="p-3 border border-gray-300">Description</th>
                 <th className="p-3 border border-gray-300">Total Cost</th>
                 <th className="p-3 border border-gray-300">Admin Remarks</th>
                 <th className="p-3 border border-gray-300">Actions</th>
@@ -103,7 +101,6 @@ const AdminInvoiceView = () => {
                   <td className="p-3 border border-gray-300">{invoice.serviceID}</td>
                   <td className="p-3 border border-gray-300">{invoice.customerName}</td>
                   <td className="p-3 border border-gray-300">{invoice.section}</td>
-                  <td className="p-3 border border-gray-300">{invoice.description}</td>
                   <td className="p-3 border border-gray-300">Rs. {invoice.totalCost.toFixed(2)}</td>
                   <td className="p-3 border border-gray-300">{invoice.adminRemarks}</td>
                   <td className="p-3 border border-gray-300">
