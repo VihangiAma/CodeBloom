97ea5201dca0351379ff9f45a827900c7fe19084
H.M.D. Anuruddhika <143576896+AnuHewage@users.noreply.github.com>
2025-07-10 11:53:34 +0530

    Create RepairPackage.js

---
+++ Nimal_Motors_Backend/Models/RepairPackage.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const repairPackageSchema = new mongoose.Schema({
+  packageName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  repairs: [
+    {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("RepairPackage", repairPackageSchema);
+
