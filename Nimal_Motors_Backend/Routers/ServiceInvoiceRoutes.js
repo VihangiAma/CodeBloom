120653f953249f0b3febf1eb96db27631b5007bf
H.M.D. Anuruddhika <143576896+AnuHewage@users.noreply.github.com>
2025-07-10 19:53:56 +0530

    Update ServiceInvoiceRoutes.js

--- Nimal_Motors_Backend/Routers/ServiceInvoiceRoutes.js
+++ Nimal_Motors_Backend/Routers/ServiceInvoiceRoutes.js
@@ -1,38 +1,4 @@
-// import express from 'express';
-// import {
-//   createInvoice,
-//   getAllInvoices,
-//   getInvoiceById,
-//   getApprovedInvoices,
-//   updateInvoiceApproval,
-//   deleteInvoice,
-//   // updateInvoice
-// } from '../Controllers/ServiceInvoiceController.js';
 
-// const router = express.Router();
-
-// //  Create a new invoice
-// router.post('/', createInvoice);
-
-// // Get only approved invoices (Must be BEFORE '/:id')
-// router.get('/approved', getApprovedInvoices);
-
-// //  Get all invoices
-// router.get('/', getAllInvoices);
-
-// // Get invoice by ID
-// router.get('/:id', getInvoiceById);
-
-// //  Approve or update approval status
-// router.put('/:id/approve', updateInvoiceApproval);
-
-// //  Delete invoice
-// router.delete('/:id', deleteInvoice);
-
-// // // Update full invoice data
-// // router.put('/:id', updateInvoice);
-
-// export default router;
 import express from "express";
 import {
   createInvoice,
