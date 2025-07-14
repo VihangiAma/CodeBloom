0cc9f9dbc47f179cbb685977638f8858b98ccb80
H.M.D. Anuruddhika <143576896+AnuHewage@users.noreply.github.com>
2025-07-15 00:48:38 +0530

    Repair Packages Updated

--- Nimal_Motors_Backend/Models/RepairPackage.js
+++ Nimal_Motors_Backend/Models/RepairPackage.js
@@ -21,26 +21,3 @@ const repairPackageSchema = new mongoose.Schema({
 });
 
 export default mongoose.model("RepairPackage", repairPackageSchema);
-
-// import mongoose from "mongoose";
-
-// const repairSchema = new mongoose.Schema({
-//   label: { type: String, required: true, trim: true },
-//   price: { type: Number, default: 0 }, // price is optional
-// });
-
-// const repairPackageSchema = new mongoose.Schema({
-//   packageName: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//     trim: true,
-//   },
-//   repairs: [repairSchema],
-//   createdAt: {
-//     type: Date,
-//     default: Date.now,
-//   },
-// });
-
-// export default mongoose.model("RepairPackage", repairPackageSchema);
--- Nimal_Motors_Backend/Routers/repairPackageRoutes.js
+++ Nimal_Motors_Backend/Routers/repairPackageRoutes.js
@@ -14,19 +14,3 @@ router.put("/:id", updateRepairPackage);
 router.delete("/:id", deleteRepairPackage);
 
 export default router;
-// import express from "express";
-// import {
-//   getAllPackages,
-//   createPackage,
-//   updatePackageRepairs,
-//   deletePackage,
-// } from "../Controllers/repairPackageController.js";
-
-// const router = express.Router();
-
-// router.get("/", getAllPackages);
-// router.post("/", createPackage);
-// router.put("/:id", updatePackageRepairs);
-// router.delete("/:id", deletePackage);
-
-// export default router;
